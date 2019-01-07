9cbb445cbb67fa9551512ec7398c83a17c811b82
David Mason <djmot@users.noreply.github.com>
2019-01-06 21:41:06 -0500

    set base url to search page

--- scrabble_search/urls.py
+++ scrabble_search/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search/', include('word_search.urls'))
+    path('', include('word_search.urls'))
 ]
